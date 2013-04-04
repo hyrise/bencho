48178bb29ffdbedef62bdaacbce1b8ca34930b69
Leander Neiss <Leander.Neiss@student.hpi.uni-potsdam.de>
2013-04-04 15:34:34 +0200

    fixed undeclared identifier error using strncmp

--- include/Gnup.h
+++ include/Gnup.h
@@ -3,6 +3,7 @@
 #define GNUP_H
 
 #include <stdlib.h>
+#include <string.h>
 
 #include <iostream>
 #include <fstream>
--- include/Pyp.h
+++ include/Pyp.h
@@ -3,6 +3,7 @@
 #define PYP_H
 
 #include <stdlib.h>
+#include <string.h>
 
 #include <string>
 #include <vector>
