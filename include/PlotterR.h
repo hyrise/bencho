e702f47ab3b2987fc54a94040e6a1e1629342411
Leander Neiss <Leander.Neiss@student.hpi.uni-potsdam.de>
2013-05-15 14:21:12 +0200

    Added doxygen documentation for PlotterR class

--- include/PlotterR.h
+++ include/PlotterR.h
@@ -1,12 +1,37 @@
+/** @file PlotterR.h
+ *
+ * Contains the class definition of PlotterR.
+ * For any undocumented method see AbstractPlotter.
+ * @see AbstractPlotter
+ *
+ */
+
 #ifndef PLOTTERR_H
 #define PLOTTERR_H
 
 
 #include "AbstractPlotter.h"
 
+
+/**
+ * @brief Class for plotting results with R ggplot2.
+ *
+ * PlotterR is derived from AbstractPlotter
+ * and serves as a plotter class for plotting results with R ggplot2.
+ */
 class PlotterR : public AbstractPlotter
 {
-public:
+private:
+
+	/**
+	 * Calls the command for executing the plotting with R.
+	 *
+	 * @param resultDir Directory where result files of the benchmark are stored.
+	 * @param plotterScript Relative path to the individual plotting script for the benchmark.
+	 * @param systemScript Relative path to the general plotting script for the plotter.
+	 * @param benchName Name of the benchmark that will be plotted.
+	 * @param benchId Id of the benchmark that will be plotted.
+	 */
 	void callPlot(string resultDir, string plotterScript, string systemScript, string benchName, string benchId);
 };
 
